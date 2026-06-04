d38944bc2a49663947146ebb8fb446a5a756742e
Tu Nombre <tu-email@ejemplo.com>
2026-06-04 14:27:11 -0600

    Fix article routes

--- config/routes.php
+++ config/routes.php
@@ -60,6 +60,10 @@ return function (RouteBuilder $routes): void {
         /*
          * ...and connect the rest of 'Pages' controller's URLs.
          */
+        $builder->connect('/articles/add', [
+    'controller' => 'Articles',
+    'action' => 'add'
+]);
         $builder->connect('/articles/{slug}',
         ['controller' => 'Articles', 'action' => 'view']
         )
